d5dddf62f85062988702d03fe5654775205dc719
chenkunnamedJack <418494114@qq.com>
2017-11-02 01:16:15 +0800

    no message

---
+++ ohNo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  ohNo
+//
+//  Created by 白朕玮 on 2017/11/1.
+//  Copyright © 2017年 白朕玮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self creatUI];
+    
+}
+
+- (void) creatUI{
+    
+    UIButton *testBtn = [[UIButton alloc]initWithFrame:]
+    
+    
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
